d1b2c0692604d281d3281fa666198e66d62946a3
Divor <1063513670@qq.com>
2022-06-27 20:41:23 +0800

    20220627 springboot集成nacos与单元测试

--- gatway-service/src/main/java/com/packet/gatway/GatwayApplication.java
+++ gatway-service/src/main/java/com/packet/gatway/GatwayApplication.java
@@ -2,8 +2,12 @@ package com.packet.gatway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient(autoRegister = true)
+@EnableCaching
 public class GatwayApplication {
 
     public static void main(String[] args) {

2b8a4665d8cdb04d4aa8444a05050fb65a34cc06
Divor <1063513670@qq.com>
2022-09-24 16:55:05 +0800

    20220924 整合hutool和swagger

--- bin-mktcenter/src/main/java/com/packet/mktcenter/MktcenterApplication.java
+++ bin-mktcenter/src/main/java/com/packet/mktcenter/MktcenterApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient(autoRegister = true)
 @EnableCaching
+@EnableSwagger2
 public class MktcenterApplication {
 
     public static void main(String[] args) {
--- bin-mktcenter/src/main/java/com/packet/mktcenter/manage/snowFlowDemo/controller/SnowFlowController.java
+++ bin-mktcenter/src/main/java/com/packet/mktcenter/manage/snowFlowDemo/controller/SnowFlowController.java
@@ -6,6 +6,10 @@ import com.packet.mktcenter.system.sysCustomAnnotation.customLog.service.SystemC
 import com.packet.mktcenter.system.sysCustomAnnotation.paramCheck.service.SysParamCheck;
 import com.packet.mktcenter.system.sysResult.model.RV;
 import com.packet.mktcenter.system.sysResult.model.ResultVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 import com.alibaba.fastjson.JSON;
 
-
+@Api("雪花算法管理相关接口")
 @Slf4j
 @RestController
 @RequestMapping(value = {"/snowFlow/SnowFlowManager"})
@@ -29,6 +33,12 @@ public class SnowFlowController {
      * @return
      */
     //@SysParamCheck(paramCheckType = "1")
+    @ApiOperation("生成了趋势递增主键")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "workerId", value = "工作机器ID", required = true), //required为是否必填项
+            @ApiImplicitParam(name = "datacenterId", value = "数据中心ID", required = true),
+            @ApiImplicitParam(name = "sequence", value = "代表一毫秒内生成的多个id的最新序号", required = true)
+    })
     @SystemCrmlog(description = "生成了趋势递增主键", tableName = "sys_snow_flow_info")
     @RequestMapping(value = {"/createSnowFlow"}, method = RequestMethod.POST)
     public ResultVO createSnowFlow(@RequestBody Map<String, String> map){
